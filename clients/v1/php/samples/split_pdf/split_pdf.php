bcc50d177a13d101200452fdbd11ccacdb71a829
Jeroen Ritmeijer <jritmeijer@muhimbi.com>
2017-03-10 15:12:41 +0000

    Added PHP Sample code and README for splitting PDF files.

---
+++ clients/v1/php/samples/split_pdf/split_pdf.php
@@ -0,0 +1,81 @@
+<?php
+
+// ** Make sure all objects associated with the Muhimbi client can be resolved
+require_once(__DIR__ . '/../../client/autoload.php');
+
+$api_key = '';
+
+//** Was a file uploaded?
+if($_FILES["file"]['size'] > 0)
+{
+    //** Make sure an api key has been entered
+    if($api_key == '') {
+        echo 'Please update the sample code and enter the API Key that came with your subscription.';
+        exit;
+    }
+
+    // ** Specify the API key associated with your subscription.
+    MuhimbiPDFOnline\Client\Configuration::getDefaultConfiguration()->setApiKey('api_key', $api_key);
+
+    // ** The service's host name is already set, but for debugging purposes you may want to switch between 'http' and 'https'.'
+    MuhimbiPDFOnline\Client\Configuration::getDefaultConfiguration()->setHost('https://api.muhimbi.com/api');
+
+    // ** We are dealing with the Splitting Api, so instantiate the relevant class
+    $api_instance = new MuhimbiPDFOnline\Client\Api\SplitApi();
+
+    // ** We need to fill out the data for the Split operation
+    $input_data = new MuhimbiPDFOnline\Client\Model\SplitPdfData();
+
+    // ** Always pass the name of the input file, or if unknown pass any name, but with the correct file extension.
+    $input_data->setSourceFileName($_FILES["file"]["name"]);
+
+    // ** Pass the content of the uploaded file, making sure it is base64 encoded.
+    $input_data->setSourceFileContent(base64_encode(file_get_contents($_FILES["file"]["tmp_name"])));
+
+    // ** We want to split based on the number of pages.
+    $input_data->setFileSplitType($input_data::FILE_SPLIT_TYPE_BY_NUMBER_OF_PAGES);
+
+    // ** In this example in batches of 2 pages per PDF file.
+    $input_data->setSplitParameter(2);
+
+    //** If you are expecting long running operations then consider longer timeouts
+    //** Also keep an eye on the maximum upload size in your php.ini (e.g. post_max_size = 10M, upload_max_filesize = 10M)
+    ini_set('default_socket_timeout', 300);
+    set_time_limit ( 300 );
+
+    try { 
+        // ** Carry out the split operation
+        $result = $api_instance->splitPdf($input_data);
+
+        // ** Send one of the split files back to the user
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Content-type: application/octet-stream");
+        header("Content-Disposition: attachment; filename=\"split.pdf\"");
+
+        // ** If we got 2 or more files back then, in this demo, send the 2nd generated file, otherwise send the first
+        if(count($result->getProcessedFileContents()) >= 2)
+            echo base64_decode($result->getProcessedFileContents()[1]);
+        else
+            echo base64_decode($result->getProcessedFileContents()[0]);
+
+        exit;
+    } catch (Exception $e) {
+        echo 'Exception when calling API: ', $e->getMessage(), PHP_EOL;
+    }
+}
+?>
+
+<html>
+  <body>
+    <form action="split_pdf.php" method="post" enctype="multipart/form-data">
+
+      <p>Please specify the PDF file to split.</p>
+      <input type="file" name="file" />
+
+      <br/><br>
+      <p>In this example the resulting PDF will be split into PDF files of 2 pages.<br/>
+      As we can only return a single file to the browser we return the 2nd generated file.</p>
+      <input type="submit" value="Split" />
+    </form>
+  </body>
+</html>
