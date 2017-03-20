12640b37d9da9794e0b5edacc53157e93451ba76
Jeroen Ritmeijer <jritmeijer@muhimbi.com>
2017-03-20 11:11:45 +0000

    C# Sample code and README for applying complex watermarks.

---
+++ clients/v1/csharp/samples/watermarking/composite_watermark/Program.cs
@@ -0,0 +1,78 @@
+﻿using Muhimbi.PDF.Online.Client.Api;
+using Muhimbi.PDF.Online.Client.Client;
+using Muhimbi.PDF.Online.Client.Model;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace sample
+{
+    class Program
+    {
+        static string API_KEY = "";
+        
+        static void Main(string[] args)
+        {
+            string testFile = null;
+
+            try
+            {
+                // ** Make sure an api key has been entered
+                if (API_KEY == string.Empty)
+                {
+                    Console.WriteLine("[ERROR] Please update the sample code and enter the API Key that came with your subscription.");
+                    return;
+                }
+
+                // ** Was a 'file to watermark' specified on the command line?
+                if (args.Count() == 0)
+                {
+                    Console.WriteLine("[INFO] No file to watermark specified, using default file.");
+                    testFile = Directory.GetFiles(".", "*.pdf")[0];
+                }
+                else
+                    testFile = args[0];
+
+                // ** Specify the API key associated with your subscription.
+                Configuration.Default.AddApiKey("api_key", API_KEY);
+
+                // ** Accept all SSL Certificates, this makes life under mono a lot easier. This line is not needed on Windows
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+                // ** The service's host name is already set, but for debugging purposes you may want to switch between 'http' and 'https'.'
+                Configuration.Default.ApiClient.RestClient.BaseUrl = new Uri("https://api.muhimbi.com/api");
+
+                // ** We are dealing with watermarking, so instantiate the relevant class
+                WatermarkApi watermarkAPI = new WatermarkApi();
+                
+                // ** Read the file we wish to watermark
+                byte[] sourceFile = File.ReadAllBytes(testFile);
+
+                // ** Fill out the data for the security operation.
+                CompositeWatermarkData inputData = new CompositeWatermarkData(
+                    SourceFileName: testFile,                           // ** Always specify a file name with the correct extension (file must be a PDF file when watermarking)
+                    SourceFileContent: sourceFile,                      // ** The file content to watermark
+                    WatermarkData: File.ReadAllText("watermark.xml")    // ** Load the XML Definition for the composite watermark
+                    );
+
+                // ** Watermark the file
+                Console.WriteLine("[INFO] Watermarking...");
+                var response = watermarkAPI.CompositeWatermark(inputData);
+
+                // ** Write the results back to the file system
+                File.WriteAllBytes(@"result.pdf", response.ProcessedFileContent);
+
+                Console.WriteLine("[INFO] 'result.pdf' written to output folder.");
+
+                // ** On Windows open the generated file in the system PDF viewer
+                Process.Start(@"result.pdf");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
