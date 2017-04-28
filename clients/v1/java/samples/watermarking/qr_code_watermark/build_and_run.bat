499be806ac4ff1c54924f12d80aac80a7455cbeb
Jeroen Ritmeijer <jritmeijer@muhimbi.com>
2017-04-28 18:31:47 +0100

    Sample code and README for applying QR codes as a watermark using Java

---
+++ clients/v1/java/samples/watermarking/qr_code_watermark/build_and_run.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo [INFO] Removing old files
+del /F /Q *.class 2>NUL
+del /F /Q result.pdf 2>NUL
+
+echo [INFO] Starting Build
+javac -cp "..\..\..\client\lib\*;." *.java
+
+if errorlevel 1 (
+   echo [ERROR] Compilation failed
+   exit /b
+)
+echo [INFO] Executable created successfully
+
+echo [INFO] Running 
+java -cp "..\..\..\client\lib\*;." sample
